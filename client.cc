f0b8db175af0632f2f2849f9f409d75f08e66506
Wenshu Novak-Condy <novakwenshu@gmail.com>
2023-03-25 15:25:40 -0400

    First commit, just created everything

---
+++ client.cc
@@ -0,0 +1,9 @@
+// Copright 2023 William Novak-Condy
+#include <iostream>
+#include <sys/ipc.h>
+
+int main (int argc, char *argv[]) {
+  
+}
+
+https://www.geeksforgeeks.org/ipc-shared-memory/
---
+++ server.cc
@@ -0,0 +1,4 @@
+// Copright 2023 William Novak-Condy
+int main (int argc, char *argv[]) {
+    
+}
