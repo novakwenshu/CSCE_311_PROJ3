788fd8f76b69edb2866709fc28956ade3e2e8224
Wenshu <novakwenshu@gmail.com>
2023-03-25 17:49:57 -0400

    got them communicating mostly added some of the basic string seperating stuff

--- client.cc
+++ client.cc
@@ -1,9 +1,37 @@
 // Copright 2023 William Novak-Condy
 #include <iostream>
 #include <sys/ipc.h>
+#include <stdio.h>
+#include <sys/shm.h>
+#include <cstring>
 
+#define SIZE 4096
+
+using namespace std;
+ 
 int main (int argc, char *argv[]) {
-  
-}
 
-https://www.geeksforgeeks.org/ipc-shared-memory/
+  key_t key = ftok("test.txt", 40);
+  int shmid = shmget(key, SIZE, 0644 | IPC_CREAT); 
+  char *mem_block = (char*) shmat(shmid, (void*)0,0);
+
+  int arglen = 0;
+
+  for (int i = 1; i < argc; i++) {
+    arglen += strlen(argv[i] + 1);
+  }
+
+  char *args = (char*) malloc(sizeof(char)*arglen);
+  strcpy(args, argv[1]);
+
+  for (int i = 2; i < argc; i++) {
+    strcat(args, "_");
+    strcat(args, argv[i]);
+  }
+  strcat(args, "\0");
+  // Sends the important info from agrv to the shared memory
+  strncpy(mem_block, args, SIZE);
+
+
+  //shmctl(shmid, IPC_RMID, NULL);
+}
--- server.cc
+++ server.cc
@@ -1,4 +1,51 @@
 // Copright 2023 William Novak-Condy
+#include <iostream>
+#include <sys/ipc.h>
+#include <stdio.h>
+#include <sys/shm.h>
+#include <cstring>
+
+using namespace std;
+
+#define SIZE 4096
+
 int main (int argc, char *argv[]) {
+  key_t key = ftok("test.txt", 0);
+  int shmid = shmget(key, SIZE, 0666|IPC_CREAT);
+  char *mem_block = (char*) shmat(shmid,  (void*)0,0);
+
+  cout << "Read this: " << mem_block << endl;
+
+  char **keywords = (char**) malloc(sizeof(char*)* strlen(mem_block)+1);
+  char *arg = (char*) malloc(sizeof(char)* strlen(mem_block)+1);
+  char *op = (char*) malloc(sizeof(char) * 2);
+  FILE* file;
+
+  arg = strtok(mem_block, "_");
+  int count = 0;
+  while (arg != NULL) {
+    keywords[count] = arg;
+    arg = strtok(NULL, "_");
+    count++;
+  }
+  if (count > 3) {
+    op = keywords[2];
+  }
+
+  // Testing
+  for (int i = 0; i < count; i++) {
+    cout << keywords[i] << endl;
+  }
+
+  file = fopen(keywords[0], "r");
+  if (file != NULL) {
+    size_t len = 0;
+    char *line = NULL;
     
+    while ((getline(&line, &len, file)) != -1) {
+
+    }
+  }
+
+  shmdt(mem_block);
 }
