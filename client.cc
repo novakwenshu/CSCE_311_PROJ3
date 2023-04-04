3e20b23be6072b5ea45a6c0b7477d4d62f8d2d6f
Wenshu <novakwenshu@gmail.com>
2023-04-04 03:20:02 -0400

    added comments

--- client.cc
+++ client.cc
@@ -20,6 +20,8 @@
 
 using namespace std;
 
+// SHM = Shared Memory
+// Struct to hold info for the method called by the threads
 struct thread_info {
   char **str;
   string line;
@@ -29,7 +31,9 @@ struct thread_info {
 
 int client (int argc, char *argv[]) {
 
+  // Makes sure there is no existing memory
   shm_unlink("test.txt");
+  // STEP 1 Creates the shared memory and truncates it
   int shmid = shm_open("test.txt", O_CREAT | O_EXCL | O_RDWR, S_IRUSR | S_IWUSR);
   if (shmid == -1) {
     cout << "shm_open fail" << endl;
@@ -38,29 +42,30 @@ int client (int argc, char *argv[]) {
   if (ftruncate(shmid, sizeof(struct shmbuf)) == -1) {
     cout << "Mem not truncated" << endl;
   }
-
+  // Maps the memory
   struct shmbuf *store = static_cast<shmbuf*>(mmap(NULL, sizeof(*store), PROT_READ | PROT_WRITE, MAP_SHARED, shmid, 0));
   if (store == MAP_FAILED) {
     cout << "MAP FAILED" << endl;
   }
-
+  // Initializes the semaphores inside the struct from shmstruct.h
   if (sem_init(&store->sem1, 1, 0) == -1) {
     cout << "Error with sem_init on 1" << endl;
   }
   if (sem_init(&store->sem2, 1, 0) == -1) {
     cout << "Error with sem_init on 2" << endl;
   }
-  
 
+  // Opens the named semaphore created by the server
   sem_t *sem = sem_open("/namesem", 0, 0, 0);
 
   store->count = strlen(argv[1]);
-
+  // tell the client that it initialized the SHM
   if (sem_post(sem) == -1) {
     cout << "Named sem post error" << endl;
   }
-  // Sends the important info from agrv to the shared memory
+  // STEP 2 copies the file name into the SHM
   memcpy(&store->buffer, argv[1], strlen(argv[1]));
+  // Waits until the server has opened the file to proceed
   if (sem_wait(&store->sem1) == -1) {
     cout << "sem_wait error" << endl;
   }
@@ -71,10 +76,11 @@ int client (int argc, char *argv[]) {
   int lineCount = 1;
 
   while (true) {
+    // Waits for the server to copy lines from the file into the SHM
     if (sem_wait(&store->sem1) == -1) {
       cout << "Error with sem_wait when reading mem" << endl;
     }
-    
+    // STEP 3 Obtains 4 or less lines from SHM and puts them in a vector
     vector<string> lines;
     for (int i = 0; i < 4; i++) {
       char line[BUF_SIZE];
@@ -82,6 +88,7 @@ int client (int argc, char *argv[]) {
       lines.push_back(line);
     }
 
+    // Checks if the server sent the message that it could not open the file
     int badFile = lines.at(0).compare("INVALID FILE");
     if (badFile == 0) {
       cerr << "INVALID FILE" << endl;
@@ -89,26 +96,29 @@ int client (int argc, char *argv[]) {
       munmap(store->buffer,SIZE);
       return 0;
     }
-
+    // Checks if the server has signaled that there are no more lines left in the file to send
     int stop = lines.at(0).compare("STOP");
     if (stop == 0) {
+      // STEP 6 Destroys mem
       shmctl(shmid, IPC_RMID, NULL);
       munmap(store->buffer,SIZE);
+      // STEP 7 returns 0
       return 0;
     }
     
+    // This loop ensures that the threads run in order
     for (int i = 0; i < 4; i++) {
       td[i].argNum = argc;
       td[i].str = argv;
       td[i].line = lines.at(i);
       td[i].lineNum = &lineCount;
-
+      // if threads have already been created
       if (i != 0) {
         if (pthread_join (threads[i-1], &res) != 0) {
           cout << "Pthread join error" << endl;
         }
       }
-
+      // STEP 4 Creating a thread for each line sent.
       int check = pthread_create(&threads[i], NULL, &checkLine, (void *)&td[i]);
       if (check) {
         cout << strerror(errno) << endl;
@@ -118,28 +128,31 @@ int client (int argc, char *argv[]) {
         break;
       }
     }
-    
+    // Signals the server that it is ready for more lines
     if (sem_post(&store->sem2) == -1) {
       cout << "Error in sem_post while reading lines from shared memory" << endl;
     }
   }
-  
+  // If some how it breaks the while loop it destoys the memory
   shmctl(shmid, IPC_RMID, NULL);
   munmap(store->buffer,SIZE);
   return 0;
 }
 
-
+// Method that the threads call to check if the line should be print out
 void* checkLine (void *threadarg) {
   struct thread_info *data;
   data = (struct thread_info *) threadarg;
   if (data->argNum > 3) {
+    // If the operator is OR
     if (data->str[3][0] == '+') {
       for (int i = 2; i < data->argNum; i++) {
+        // The key words will only be in the even indices
         if(i%2 == 0) {
           // For each keyword it checks if it is contained in the string.
           // If found, it breaks the loop to avoid duplicates
           if (data->line.find(data->str[i]) != string::npos) {
+            // STEP 5 Printing lines to cout if meets the criteria based on operator
             cout << *(data->lineNum) << "\t";
             cout << data->line << endl;
             *(data->lineNum) = *(data->lineNum) + 1;
@@ -162,6 +175,7 @@ void* checkLine (void *threadarg) {
         }
       }
       if (toSend) {
+        // STEP 5 Printing lines to cout if meets the criteria based on operator
         cout << *(data->lineNum) << "\t";
         cout << data->line << endl;
         *(data->lineNum) = *(data->lineNum) + 1;
@@ -170,6 +184,7 @@ void* checkLine (void *threadarg) {
     // If there is only one argument given
   } else if (data->argNum == 3) {
     if ((data->line.find(data->str[2])) != string::npos) {
+      // STEP 5 Printing lines to cout if meets the criteria based on operator
       cout << *(data->lineNum) << "\t";
       cout << data->line << endl;
       *(data->lineNum) = *(data->lineNum) + 1;
--- server.cc
+++ server.cc
@@ -15,11 +15,14 @@
 
 using namespace std;
 
-
+// TODO DESTROY NAMED SEMPAHORE
 
 int server (int argc, char *argv[]) {
+  
+  // STEP 1
   cout << "SERVER STARTED" << endl;
   while (true) {
+    // Unlinks
     sem_unlink("/namesem");
     sem_t *sem = sem_open("/namesem", O_CREAT, S_IRGRP | S_IRUSR | S_IWGRP | S_IWUSR, 0);
 
@@ -27,79 +30,91 @@ int server (int argc, char *argv[]) {
       cout << "Named sem not instantiated" << endl;
       cout << strerror(errno) << endl;
     }
+    // Waits for the client to initialize the mem
     if (sem_wait(sem) == -1) {
       cout << "Named sem wait error" << endl;
     }
 
+    // STEP 3 opens shared memory in order to obtain path
     int shmid = shm_open("test.txt", O_RDWR, 0);
     if (shmid == -1) {
       cerr << "shm_open " << strerror(errno) << endl;
     }
     
-    // May not need to be static casted
+    // MAPS THE MEMORY
     struct shmbuf *store = static_cast<shmbuf*>(mmap(nullptr, sizeof(*store), PROT_READ | PROT_WRITE, MAP_SHARED, shmid, 0));
     if (store == MAP_FAILED) {
         cout << "MAP FAILED" << endl;
         cerr << "mmap " << strerror(errno) << endl;
     }
     
-
+    // STEP 2 Gets the file name from the client and uses it to open the file
     char *path = (char*) malloc(sizeof(char)* strlen(store->buffer)+1);
     FILE* file;
     path = store->buffer;
-    cout << store->buffer << endl;
 
     clog << "CLIENT REQUEST RECIEVED" << endl;
     clog << "\tMEMORY OPEN" << endl;
 
+    // STEP 4 Opening the file
     file = fopen(path, "r");
     int count = 0;
+    // Tells client that it has read the file name
     if (sem_post(&store->sem1) == -1) {
     cout << "sem_wait error" << endl;
     }
     
     if (file != NULL) {
+      // If the file is valid
       clog << "\tOPENING: " << path << endl;
       size_t len = 0;
       char *line = NULL;
       while ((getline(&line, &len, file)) != -1) {
         if ((count != 0 && count%4 == 0)) {
+          // Once 4 lines have been put into shared memory, tell client that it can read it
           if (sem_post(&store->sem1) == -1) {
             cout << "Error in sem_wait while putting lines in shared memory" << endl;
           }
+          // Wait until client is done processing the lines
           if (sem_wait(&store->sem2) == -1) {
             cout << "Error in sem_post while putting lines in shared memory" << endl;
           }
+          // Clears the buffer
           memset(store->buffer, '\0', sizeof(store->buffer));
         }
-        // If error maybe check if sending null terminator since no + 1
+        // Depending on the line offsets where it is in SHM
         memcpy(&store->buffer[(count%4)*1024], line, strlen(line)+1);
-        char test[BUF_SIZE];
-        strncpy(test,&store->buffer[count%4*OFFSET], OFFSET);
         count++;
       }
-      // One last post to get the last lines over
+      // One last post to get the leftover lines over
       if (sem_post(&store->sem1) == -1) {
         cout << "Error in sem_wait while putting lines in shared memory" << endl;
       }
       if (sem_wait(&store->sem2) == -1) {
         cout << "Error in sem_post while putting lines in shared memory" << endl;
       }
+      // Clears
       memset(store->buffer, '\0', sizeof(store->buffer));
+      // Closes the file since all lines have been sent
       fclose(file);
       clog << "\tFILE CLOSED" << endl;
+      // This means the file was invalid
     } else if (file == NULL) {
+        // Sends thats the file was invalid to server
         memcpy(&store->buffer[0], "INVALID FILE", strlen("INVALID FILE")+1);
+        // Tells the server that it can see what in the SHM
         if (sem_post(&store->sem1) == -1) {
           cout << "Error in sem_wait while putting lines in shared memory" << endl;
         }
     }
 
+    // After everything is done, this tells the client to stop
     memcpy(&store->buffer[0], "STOP", strlen("STOP")+1);
     if (sem_post(&store->sem1) == -1) {
       cout << "Error with sem_wait when reading mem" << endl;
     }
 
+    // Closes memory
     shmctl(shmid, IPC_RMID, NULL);
     munmap(store->buffer,4096);
     clog << "\tMEMORY CLOSED" << endl;
